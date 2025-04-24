33e9ff6109ead3c6fd4e849c9229e7c1ecc7152b
LuoYuNing <2674795507@qq.com>
2025-04-24 22:19:52 +0800

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 (function($){
   $(function(){
-    // 添加新的点击事件处理
-    $('#menu-toggle-button').on('click', function(e) {
-      e.preventDefault(); // 阻止链接默认行为
-      $('body').toggleClass('sidebar-open'); // 切换 body 的 class
-    });
+    $(".button-collapse").sideNav();
   });
 })(jQuery);
